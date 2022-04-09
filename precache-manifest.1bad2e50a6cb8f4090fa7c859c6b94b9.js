59d1bb728c2fa20e3634ed65a5bd4567895deb92
kamran1007 <54858509+kamran1007@users.noreply.github.com>
2022-04-09 06:09:32 +0530

    Updates

---
+++ precache-manifest.1bad2e50a6cb8f4090fa7c859c6b94b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c991f0c12336c68b57747f2fc613db89",
+    "url": "/kamran1007/covid_19tracker.github.io/index.html"
+  },
+  {
+    "revision": "c409f1f9e375aa2097b0",
+    "url": "/kamran1007/covid_19tracker.github.io/static/css/main.d09b88ee.chunk.css"
+  },
+  {
+    "revision": "8ba2de8d141f137ee878",
+    "url": "/kamran1007/covid_19tracker.github.io/static/js/2.682b037d.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/kamran1007/covid_19tracker.github.io/static/js/2.682b037d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c409f1f9e375aa2097b0",
+    "url": "/kamran1007/covid_19tracker.github.io/static/js/main.a16d6dbe.chunk.js"
+  },
+  {
+    "revision": "ec4d85f993ac6860c5d9",
+    "url": "/kamran1007/covid_19tracker.github.io/static/js/runtime-main.9dc8e9c1.js"
+  },
+  {
+    "revision": "dbc3dfc9276547258d4b47182cd00bfd",
+    "url": "/kamran1007/covid_19tracker.github.io/static/media/covid.dbc3dfc9.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/kamran1007/covid_19tracker.github.io/precache-manifest.1bad2e50a6cb8f4090fa7c859c6b94b9.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kamran1007/covid_19tracker.github.io/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
